497f43fadd43b7c48372379c63b6d193ca2c444c
Mallidi Akhil Reddy <akhilmallidi.98@gmail.com>
2022-02-07 21:46:37 +0530

    added: header component responsive to all devices

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -16,20 +16,29 @@ const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.container}>
-        <div className={styles.imgContainer}>
+        <div className={`${styles.imgContainer} ${styles.lgDevice}`}>
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png"
+            layout="fill"
+            objectFit="contain" />
+        </div>
+        <div className={`${styles.imgContainer} ${styles.smDevice}`}>
+          <Image
+            src="https://1000logos.net/wp-content/uploads/2017/02/insta-logo.png"
             layout="fill"
             objectFit="contain" />
         </div>
         <SearchField />
         <div className={styles.navBtns}>
           <HomeIcon />
-          <MenuIcon />
-          <PaperAirplaneIcon />
-          <PlusCircleIcon />
-          <UserGroupIcon />
-          <HeartIcon />
+          <MenuIcon className={styles.smBtn}/>
+          <div className={styles.msgBtn}>
+            <PaperAirplaneIcon />
+            <div className={styles.count}>2</div>
+          </div>
+          <PlusCircleIcon className={styles.lgBtn} />
+          <UserGroupIcon className={styles.lgBtn} />
+          <HeartIcon className={styles.lgBtn} />
         </div>
       </div>
     </div>
